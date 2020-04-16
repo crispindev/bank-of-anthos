1c4ee44295f0344e83a529d3a8fffe82c90457fc
bharathkkb <bharathkrishnakb@gmail.com>
2020-04-15 19:34:29 -0500

    clean deps, add venv to sh script

--- .github/workflows/install-dependencies.sh
+++ .github/workflows/install-dependencies.sh
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # install git, kubectl, maven, jdk, pip
-sudo apt-get -y install git kubectl maven default-jdk python3-pip
+sudo apt-get -y install git kubectl maven default-jdk python3-pip python3-venv
 
 # install go
 curl -O https://storage.googleapis.com/golang/go1.12.9.linux-amd64.tar.gz
